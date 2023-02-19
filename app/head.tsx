a83a1eb06272b34cbdde733f9d08fab999a041bf
dylanbr0wn <dylan@hololabs.org>
2023-02-19 00:27:17 -0800

    some more wacky things

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
 	return (
 		<>
-			<title>Create Next App</title>
+			<title>todos</title>
 			<meta
 				content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0"
 				name="viewport"
--- components/page-resizer.tsx
+++ components/page-resizer.tsx
@@ -1,12 +1,29 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 function PageResizer() {
 	useEffect(() => {
 		function setSize() {
+			let portraitMode = window.innerHeight > window.innerWidth
 			var vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
 			document.documentElement.style.setProperty('--vh', `${vh}px`)
+			if (
+				(portraitMode === true && window.innerHeight < window.innerWidth) ||
+				(portraitMode === false && window.innerHeight > window.innerWidth)
+			) {
+				portraitMode = window.innerHeight > window.innerWidth
+				document
+					.querySelector('meta[name=viewport]')
+					?.setAttribute(
+						'content',
+						'width=' +
+							window.innerWidth +
+							', height=' +
+							window.innerHeight +
+							', initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+					)
+			}
 		}
 		setSize()
 		window.addEventListener('resize', setSize)
