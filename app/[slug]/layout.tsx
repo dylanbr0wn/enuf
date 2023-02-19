bc906250427cf7355279805388ec7ae9a98266b7
dylanbr0wn <dylan@hololabs.org>
2023-02-19 00:11:57 -0800

    adjust body size instead?

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -3,7 +3,7 @@ import { PageResizer } from '@/components/page-resizer'
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<main
-			className="main mx-auto grid w-full gap-1 pt-[4.25rem]"
+			className="mx-auto grid h-full w-full gap-1 pt-[4.25rem]"
 			style={{ gridTemplateRows: '1fr auto' }}
 		>
 			{children}
