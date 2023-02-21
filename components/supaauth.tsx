fc801c30f6c7882d316058d1b74dc87055d916bf
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2023-02-20 20:21:38 -0800

    supabase auth

--- components/supaauth.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { supabase } from '@/lib/supabase'
-import { Auth } from '@supabase/auth-ui-react'
-
-function SupaAuth() {
-	return <Auth supabaseClient={supabase} providers={['notion']} />
-}
-export { SupaAuth }
